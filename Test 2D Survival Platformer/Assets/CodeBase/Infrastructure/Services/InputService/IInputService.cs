2a87de4485b4c8bf0204cf7fdd8a1c73c12feda3
Michael Dvin <idewishortcut@gmail.com>
2024-07-07 11:44:51 +0500

    Import UniRx

--- Test 2D Survival Platformer/Assets/CodeBase/Infrastructure/Services/InputService/IInputService.cs
+++ Test 2D Survival Platformer/Assets/CodeBase/Infrastructure/Services/InputService/IInputService.cs
@@ -1,7 +1,10 @@
-﻿namespace CodeBase.Infrastructure.Services.InputService
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.Services.InputService
 {
     public interface IInputService
     {
-        
+        event Action<float> HorizontalMoveInput;
     }
 }
--- Test 2D Survival Platformer/Assets/CodeBase/Infrastructure/Services/InputService/InputService.cs
+++ Test 2D Survival Platformer/Assets/CodeBase/Infrastructure/Services/InputService/InputService.cs
@@ -1,7 +1,20 @@
-﻿namespace CodeBase.Infrastructure.Services.InputService
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace CodeBase.Infrastructure.Services.InputService
 {
-    public class InputService : IInputService
+    public class InputService : IInputService, ITickable
     {
+        public event Action<float> HorizontalMoveInput;
         
+        
+        public void Tick()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            
+            if (horizontal != 0) 
+                HorizontalMoveInput?.Invoke(horizontal);
+        }
     }
 }
