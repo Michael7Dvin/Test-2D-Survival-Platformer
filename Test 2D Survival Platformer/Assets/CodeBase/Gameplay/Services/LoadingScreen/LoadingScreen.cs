2352633c6f96df18f6d86c40c94a3abdf699eabe
Michael Dvin <idewishortcut@gmail.com>
2024-07-16 17:26:15 +0500

    Add dimming for DeathWindow

---
+++ Test 2D Survival Platformer/Assets/CodeBase/Gameplay/Services/LoadingScreen/LoadingScreen.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeBase.Gameplay.Services.LoadingScreen
+{
+    public class LoadingScreen : ILoadingScreen
+    {
+        public void Initialize()
+        {
+            
+        }
+
+        public void Show()
+        {
+        }
+
+        public void Hide()
+        {
+        }
+    }
+
+    public interface ILoadingScreen
+    {
+        void Initialize();
+        void Show();
+        void Hide();
+    }
+}
